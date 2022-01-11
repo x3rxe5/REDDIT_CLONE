bf7e82d92c7dd71d4f48dd9238fb50c781d00ddf
xerxes <sharmapulkit310@gmail.com>
2022-01-11 13:23:21 +0530

    ssr updated

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-## Reddit Clone
+# Reddit Clone
+### 
+<br />
+<hr />
+
+Table of contents
+=================
+
+
+<!--ts-->  
+   * [Installation](#installation)
+        * [backend](#backend)
+	        * [application](#application)
+	        * [database](#database)
+        * [frontend](#frontend)
+<!--te-->
+
+
+
+Installation
+============
+
+backend
+-------
+
+<h4>application </h4>
+
+```bash
+$ cd backend/
+
+$ yarn watch && yarn dev
+```
+
+<h4> Database </h4>
+
+<h6> database:Postgresql </h6>
+```bash
+$ yarn create:migration
+```
+
+
+<h4> Both </h4>
+```bash
+$ yarn fire #or
+$ npm fire
+```
+
+
+frontend
+-------
+
+```bash
+$ cd frontend/
+
+# for installing dependency
+$ npm install # or
+$ yarn install
+
+# and then run the following command
+$ npm run start
+```
--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -132,6 +132,11 @@ export type MeQueryVariables = Exact<{ [key: string]: never; }>;
 
 export type MeQuery = { __typename?: 'Query', me?: { __typename?: 'Users', id: number, username: string, email: string } | null | undefined };
 
+export type PostsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type PostsQuery = { __typename?: 'Query', posts: Array<{ __typename?: 'Post', id: number, title: string, createdAt: string, updateAt: string }> };
+
 export const RegularUserFragmentDoc = gql`
     fragment RegularUser on Users {
   id
@@ -203,4 +208,18 @@ export const MeDocument = gql`
 
 export function useMeQuery(options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'query'> = {}) {
   return Urql.useQuery<MeQuery>({ query: MeDocument, ...options });
+};
+export const PostsDocument = gql`
+    query Posts {
+  posts {
+    id
+    title
+    createdAt
+    updateAt
+  }
+}
+    `;
+
+export function usePostsQuery(options: Omit<Urql.UseQueryArgs<PostsQueryVariables>, 'query'> = {}) {
+  return Urql.useQuery<PostsQuery>({ query: PostsDocument, ...options });
 };
